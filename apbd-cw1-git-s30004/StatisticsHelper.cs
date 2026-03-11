f8d2ceff5240406b56d3fc1177d19fd8f79071de
Derya Ogus <s30004@pjwstk.edu.pl>
2026-03-11 15:00:13 +0100

    Add CalculateAverage method

--- apbd-cw1-git-s30004/StatisticsHelper.cs
+++ apbd-cw1-git-s30004/StatisticsHelper.cs
@@ -15,5 +15,10 @@ public static class StatisticsHelper
     {
         return values.Length;
     }
+    public static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0) return 0;
+        return (double)CalculateSum(values) / values.Length;
+    }
 }
 
