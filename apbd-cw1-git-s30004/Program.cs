a02668a2e75ad53ab010bf869c7baeeb420cb81f
Derya Ogus <s30004@pjwstk.edu.pl>
2026-03-11 14:55:26 +0100

    Add basic input handling with validation

--- apbd-cw1-git-s30004/Program.cs
+++ apbd-cw1-git-s30004/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using apbd_cw1_git_s30004;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter numbers separated by spaces:");
+var input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("No input provided.");
+    return;
+}
+
+var numbers = input.Split(' ')
+    .Where(s => !string.IsNullOrWhiteSpace(s))
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine($"Sum: {StatisticsHelper.CalculateSum(numbers)}");
